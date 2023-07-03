8cde6f138a69ffe5b0459830abffcac735011234
Muqiu Han <muqiu-han@outlook.com>
2023-07-03 16:45:35 +0800

    Replace the fullwidth character to halfwidth character

--- main.py
+++ main.py
@@ -5,6 +5,21 @@ import re
 class Filter:
     """对聊天记录进行格式化"""
 
+    __FULL2HALF = dict((i + 0xFEE0, i) for i in range(0x21, 0x7F))
+    __FULL2HALF[0x3000] = 0x20
+
+    @staticmethod
+    def __halfen(chat_history: list[str]) -> list[str]:
+        """将全角字符转换为半角字符"""
+        return list(
+            map(
+                lambda history: "".join(
+                    list(map(lambda ch: ch.translate(Filter.__FULL2HALF), list(history)))
+                ),
+                chat_history,
+            )
+        )
+
     @staticmethod
     def __remove_empty(chat_history: list[str]) -> list[str]:
         """删除聊天记录中的空白记录"""
@@ -25,7 +40,9 @@ class Filter:
     @staticmethod
     def filter(strings: list[str]) -> list[str]:
         return Filter.__remove_empty(
-            Filter.__normalize_speaker_info((Filter.__remove_empty(strings)))
+            Filter.__normalize_speaker_info(
+                Filter.__halfen(Filter.__remove_empty(strings))
+            )
         )
 
 
